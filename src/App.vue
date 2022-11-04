65fc7814100f27456cb628c9d5b376bc5c83f0c2
Anwesh <anweshd2000@gmail.com>
2022-11-04 15:49:10 +0545

    Initialize Project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  metaInfo: {
+    title: 'My Podcast+',
+  },
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
